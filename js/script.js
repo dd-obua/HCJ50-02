99953c55cca6b1ca714a252f34c73f00bf24eb06
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-09 19:48:03 +0300

    Select progress divs

--- js/script.js
+++ js/script.js
@@ -3,3 +3,4 @@
 const btnPrev = document.querySelector('.btn__prev');
 const btnNext = document.querySelector('.btn__next');
 const circles = document.querySelectorAll('.circle');
+const progress = document.querySelector('.progress');
