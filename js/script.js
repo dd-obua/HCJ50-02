553c98e3c1e75be0e0dce5f4496c6e88d9d19dc0
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-09 19:49:10 +0300

    Define updateDisplay method

--- js/script.js
+++ js/script.js
@@ -6,3 +6,10 @@ const circles = document.querySelectorAll('.circle');
 const progress = document.querySelector('.progress');
 
 let curActive = 1;
+
+const updateDisplay = function () {
+  circles.forEach((circle, index) => {
+    if (index < curActive) circle.classList.add('circle--active');
+    else circle.classList.remove('circle--active');
+  });
+};
