01e737f8e34aeb17eec70b00edfefd410cc3f118
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-09 20:22:58 +0300

    Implement progress bar coloring

--- js/script.js
+++ js/script.js
@@ -11,12 +11,26 @@ const updateDisplay = function () {
   circles.forEach((circle, index) => {
     if (index < curActive) circle.classList.add('circle--active');
     else circle.classList.remove('circle--active');
+
+    const activeCircles = document.querySelectorAll('.circle--active');
+    progress.style.width = `${
+      ((activeCircles.length - 1) / (circles.length - 1)) * 100
+    }%`;
+
+    if (curActive === 1) {
+      btnPrev.disabled = true;
+    } else if (curActive === circles.length) {
+      btnNext.disabled = true;
+    } else {
+      btnPrev.disabled = false;
+      btnNext.disabled = false;
+    }
   });
 };
 
 btnNext.closest('.container').addEventListener('click', function (event) {
   const clicked = event.target;
-  
+
   if (clicked.classList.contains('btn__next')) {
     curActive++;
     if (curActive > circles.length) curActive = circles.length;
