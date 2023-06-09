cdb318701a0d5e84ba4ff3e0f67a71d470460ff4
Obua Denis Daniel <dd.obua@gmail.com>
2023-06-09 17:57:46 +0300

    Select buttons and circles

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 'use strict';
-  
+
+const btnPrev = document.querySelector('.btn__prev');
+const btnNext = document.querySelector('.btn__next');
+const circles = document.querySelectorAll('.circle');
